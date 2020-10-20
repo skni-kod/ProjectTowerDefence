614ee566c205f3f86a17b5548243b43e81127d56
Bernes <56940210+Bernes99@users.noreply.github.com>
2020-10-20 21:29:17 +0200

    Konterola kamery myszą

--- PrjectTowerDefence/Assets/Scripts/CameraControler.cs
+++ PrjectTowerDefence/Assets/Scripts/CameraControler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
@@ -7,6 +8,9 @@ public class CameraControler : MonoBehaviour
 {
     [SerializeField]
     float moveSpeed=4f;
+    [SerializeField]
+    float edgeSize=40f;
+
     Vector3 horizontal , vertical;
     void Start()
     {
@@ -17,24 +21,49 @@ public class CameraControler : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.anyKey)
         {
-            Move();   
+            CmaeraKeyMove();
+        }
+        if(Input.mousePosition.x >Screen.width-edgeSize)
+        {
+            CmaeraMouseMove(1,0);
+        }
+        if (Input.mousePosition.x < edgeSize)
+        {
+            CmaeraMouseMove(-1,0);
+        }
+        if (Input.mousePosition.y > Screen.height - edgeSize)
+        {
+            CmaeraMouseMove(0,1);
+        }
+        if (Input.mousePosition.y < edgeSize)
+        {
+            CmaeraMouseMove(0,-1);
         }
     }
-    void Move()
+
+    void CmaeraKeyMove()
     {
-        Vector3 directed = new Vector3(Input.GetAxis("horizontalKey"), 0, Input.GetAxis("verticalKey"));
+        //Vector3 directed = new Vector3(Input.GetAxis("horizontalKey"), 0, Input.GetAxis("verticalKey"));
         Vector3 horizontalMove = horizontal * moveSpeed * Time.deltaTime * Input.GetAxis("horizontalKey");
         Vector3 verticalMove = vertical * moveSpeed * Time.deltaTime * Input.GetAxis("verticalKey");
-
+        //Debug.Log(Input.GetAxis("verticalKey"));
         //Vector3 heading = Vector3.Normalize(horizontalMove + verticalMove);
 
         // transform.forward = heading;
         transform.position += horizontalMove;
         transform.position += verticalMove;
+    }
+    void CmaeraMouseMove(short directionX,short directionY)
+    {
+        Vector3 horizontalMove = horizontal * moveSpeed * Time.deltaTime * directionX;
+        Vector3 verticalMove = vertical * moveSpeed * Time.deltaTime * directionY;
+        
+        transform.position += horizontalMove;
+        transform.position += verticalMove;
+
     }
 }
