299742c8e1fe9aa133974f08fc303392fe9f4b83
Bernes <56940210+Bernes99@users.noreply.github.com>
2020-11-07 17:45:34 +0100

    Enemy Template

---
+++ ProjectTowerDefence/Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    protected float hp;
+    protected float speed;
+    protected int lvl;
+
+
+    public Enemy()
+    {
+
+    }
+
+    /// <summary>
+    /// poruszanie sie postaci
+    /// </summary>
+    protected virtual void movement()
+    {
+        Debug.Log("coś poszło nie tak, Zjebałeś");
+    }
+    /// <summary>
+    /// atak przeciwnika
+    /// </summary>
+    protected virtual void atack()
+    {
+        Debug.Log("coś poszło nie tak, Zjebałeś");
+    }
+    /// <summary>
+    /// inicjalizacja statystyk
+    /// </summary>
+    protected virtual void initStats()
+    {
+        Debug.Log("coś poszło nie tak, Zjebałeś");
+    }
+    
+}
---
+++ ProjectTowerDefence/Assets/Scripts/Goblin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Goblin : Enemy
+{
+    [SerializeField]
+    private float Hp=100f;
+    [SerializeField]
+    private float Speed = 20f;
+    
+    protected override void initStats()
+    {
+        base.hp = Hp;
+
+        base.speed = Speed;
+    }
+
+}
