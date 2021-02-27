77c7d022e8ca5d7fc0f4057cd7bed166f27bf29b
Bernes <56940210+Bernes99@users.noreply.github.com>
2021-02-27 13:49:49 +0100

    World grid

---
+++ ProjectTowerDefence/Assets/Scripts/Grid.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private int[,] gridArray;
+
+    public Grid(int width,int height,float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridArray = new int[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                CreateGridText(null, gridArray[x, y].ToString(), GetWorldPosition(x, y) + (new Vector3(cellSize,0,cellSize)/2f));
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white,10f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x+1, y), Color.white, 10f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width,height), Color.white, 10f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width,height), Color.white, 10f);
+    }
+    private TextMesh CreateGridText(Transform parent, string text, Vector3 localPosition)
+    {
+        GameObject textMeshObject = new GameObject("GridText", typeof(TextMesh));
+        
+        Transform transform = textMeshObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localPosition;
+        transform.Rotate(90, 0, 0);
+
+        TextMesh textMesh = textMeshObject.GetComponent<TextMesh>();
+        textMesh.text = text;
+        textMesh.color = Color.white;
+        textMesh.fontSize = 20;
+        textMesh.alignment = TextAlignment.Left;
+
+        return textMesh;
+    }
+
+    private Vector3 GetWorldPosition(int x ,int y )
+    {
+        return new Vector3(x,0, y) * cellSize;
+    }
+
+}
---
+++ ProjectTowerDefence/Assets/Scripts/World.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Grid worldGrid = new Grid(2, 5, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
