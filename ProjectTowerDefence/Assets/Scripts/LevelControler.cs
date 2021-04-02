62a05d87a1290ce91a149768929a6944bee00042
Adrian_99 <57730065+Adrian-99@users.noreply.github.com>
2021-04-02 09:35:01 +0200

    LevelControler comments & code cleaning

--- ProjectTowerDefence/Assets/Scripts/LevelControler.cs
+++ ProjectTowerDefence/Assets/Scripts/LevelControler.cs
@@ -4,41 +4,96 @@ using UnityEngine;
 
 public class LevelControler : MonoBehaviour
 {
+    #region Deklaracje zmiennych
+    /// <summary>
+    /// Lista pozycji, w których mają się spawnować przeciwnicy. Powinny być one ustawiane w edytorze Unity!
+    /// </summary>
+    [SerializeField]
     protected List<Vector3> enemiesSpawnPoints = new List<Vector3>();
 
+    /// <summary>
+    /// Liczba przeciwników pozostałych do zespawnowania podczas obecnej fali
+    /// </summary>
     protected int enemiesCountToSpawn;
+
+    /// <summary>
+    /// Czas ostatniego zespawnowania przeciwnika
+    /// </summary>
     protected float lastEnemySpawnTime;
+
+    /// <summary>
+    /// Odstęp czasowy pomiędzy spawnowaniem kolejnych przeciwników. Można ustawiać w edytorze Unity.
+    /// </summary>
     [SerializeField]
+    [Range(0, 30)]
     protected float enemySpawningCooldown = 2;
 
+    /// <summary>
+    /// Typ wyliczeniowy faz gry
+    /// </summary>
     public enum levelPhase { preparations, defence }
+
+    /// <summary>
+    /// Obecna faza gry
+    /// </summary>
     protected levelPhase currentPhase;
+
+    /// <summary>
+    /// Numer obecnej fali przeciwników
+    /// </summary>
     protected int currentEnemiesWave;
+
+    /// <summary>
+    /// Obecna faza gry
+    /// </summary>
     public levelPhase GetCurrentPhase { get { return currentPhase; } }
+
+    /// <summary>
+    /// Numer obecnej fali przeciwników
+    /// </summary>
     public int GetCurrentWave { get { return currentEnemiesWave; } }
 
+    /// <summary>
+    /// Lista wszystkich dostępnych prefabów przeciwników. Uwaga! Trzeba ją ręcznie aktualizować w edytorze Unity!
+    /// </summary>
     [SerializeField]
     protected List<GameObject> enemiesPrefabs = new List<GameObject>();
 
+    /// <summary>
+    /// Obiekt nadrzędny, pod którym mają być tworzone obiekty przeciwników
+    /// </summary>
     [SerializeField]
     protected Transform enemiesParentObject;
+    #endregion
 
     // Start is called before the first frame update
     void Start()
     {
+        InitVariables();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LevelPhaseActions();
+    }
+
+    /// <summary>
+    /// Początkowa inicjalizacja zmiennych
+    /// </summary>
+    private void InitVariables()
+    {
         currentPhase = levelPhase.preparations;
         currentEnemiesWave = 1;
-
-        enemiesSpawnPoints.Clear();
-        enemiesSpawnPoints.Add(new Vector3(3, 1, 11));
-        enemiesSpawnPoints.Add(new Vector3(3, 1, 7));
 
         enemiesCountToSpawn = 0;
         lastEnemySpawnTime = -enemySpawningCooldown;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Obsługa zdarzeń związanych z fazą gry
+    /// </summary>
+    private void LevelPhaseActions()
     {
         switch (currentPhase)
         {
