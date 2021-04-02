1fb700d8bb4eba2883ee9beb342e46c48c79b8fb
Adrian_99 <57730065+Adrian-99@users.noreply.github.com>
2021-04-02 10:51:44 +0200

    Using predefinied enemies waves

--- ProjectTowerDefence/Assets/Scripts/LevelControler.cs
+++ ProjectTowerDefence/Assets/Scripts/LevelControler.cs
@@ -12,9 +12,9 @@ public class LevelControler : MonoBehaviour
     protected List<Vector3> enemiesSpawnPoints = new List<Vector3>();
 
     /// <summary>
-    /// Liczba przeciwników pozostałych do zespawnowania podczas obecnej fali
+    /// Lista przeciwników pozostałych do zespawnowania podczas obecnej fali
     /// </summary>
-    protected int enemiesCountToSpawn;
+    protected List<EnemiesWave> enemiesToSpawn = new List<EnemiesWave>();
 
     /// <summary>
     /// Czas ostatniego zespawnowania przeciwnika
@@ -64,12 +64,24 @@ public class LevelControler : MonoBehaviour
     /// </summary>
     [SerializeField]
     protected Transform enemiesParentObject;
+
+    /// <summary>
+    /// Lista zestawów fal przeciwników dla różnych poziomów
+    /// </summary>
+    protected List<List<List<EnemiesWave>>> allEnemiesWaves = new List<List<List<EnemiesWave>>>();
+
+    /// <summary>
+    /// Index zestawu fal przeciwników, który mas być użyty
+    /// </summary>
+    [SerializeField]
+    protected int enemiesWavesSetId;
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
         InitVariables();
+        DefineEnemiesWaves();
     }
 
     // Update is called once per frame
@@ -84,9 +96,9 @@ public class LevelControler : MonoBehaviour
     private void InitVariables()
     {
         currentPhase = levelPhase.preparations;
-        currentEnemiesWave = 1;
+        currentEnemiesWave = 0;
 
-        enemiesCountToSpawn = 0;
+        enemiesToSpawn.Clear();
         lastEnemySpawnTime = -enemySpawningCooldown;
     }
 
@@ -103,9 +115,9 @@ public class LevelControler : MonoBehaviour
                 break;
 
             case levelPhase.defence:
-                if (enemiesCountToSpawn > 0)
+                if (enemiesToSpawn.Count > 0)
                 {
-                    if (Time.time - lastEnemySpawnTime > enemySpawningCooldown) SpawnRandomEnemy();
+                    if (Time.time - lastEnemySpawnTime > enemySpawningCooldown) SpawnNextEnemy();
                 }
                 else if (GameObject.FindGameObjectsWithTag("Enemies").Length == 0)
                 {
@@ -122,7 +134,26 @@ public class LevelControler : MonoBehaviour
     {
         Debug.Log($"Starting wave {currentEnemiesWave}");
 
-        enemiesCountToSpawn = currentEnemiesWave * 2;
+        if (enemiesWavesSetId < 0 || enemiesWavesSetId >= allEnemiesWaves.Count)
+        {
+            Debug.LogError("Nieprawidłowy numer zestawu fal przeciwników");
+            enemiesWavesSetId = 0;
+        }
+
+        int definiedWavesCount = allEnemiesWaves[enemiesWavesSetId].Count;
+        int statsMultiplier = currentEnemiesWave / definiedWavesCount + 1;
+
+        enemiesToSpawn.Clear();
+        foreach (EnemiesWave ew in allEnemiesWaves[enemiesWavesSetId][currentEnemiesWave % definiedWavesCount])
+        {
+            if (ew.count > 0)
+            {
+                for (int i = 0; i < ew.count; i++)
+                {
+                    enemiesToSpawn.Add(new EnemiesWave(ew.prefabId, ew.hp * statsMultiplier, ew.speed * statsMultiplier));
+                }
+            }
+        }
 
         currentPhase = levelPhase.defence;
     }
@@ -140,17 +171,91 @@ public class LevelControler : MonoBehaviour
     }
 
     /// <summary>
-    /// Spawnuje losowego przeciwnika
+    /// Spawnuje następnego przeciwnika
     /// </summary>
-    protected void SpawnRandomEnemy()
+    protected void SpawnNextEnemy()
     {
-        int randomPrefabIndex = Random.Range(0, enemiesPrefabs.Count);
-        int randomSpawnPointIndex = Random.Range(0, enemiesSpawnPoints.Count);
-        GameObject newEnemy = Instantiate(enemiesPrefabs[randomPrefabIndex], enemiesSpawnPoints[randomSpawnPointIndex], Quaternion.identity, enemiesParentObject);
+        if (enemiesToSpawn.Count > 0)
+        {
+            EnemiesWave enemyToSpawn = enemiesToSpawn[0];
 
-        newEnemy.GetComponent<Enemy>().InitStats(40 + currentEnemiesWave * 10, 4 + currentEnemiesWave);
+            int randomSpawnPointIndex = Random.Range(0, enemiesSpawnPoints.Count);
+            int prefabId = enemyToSpawn.prefabId;
+            if (prefabId < 0 || prefabId >= enemiesPrefabs.Count)
+            {
+                Debug.LogError("Nieprawidłowy numer prefabu przeciwnika");
+                prefabId = 0;
+            }
+            GameObject newEnemy = Instantiate(enemiesPrefabs[prefabId], enemiesSpawnPoints[randomSpawnPointIndex], Quaternion.identity, enemiesParentObject);
 
-        enemiesCountToSpawn--;
-        lastEnemySpawnTime = Time.time;
+            newEnemy.GetComponent<Enemy>().InitStats(enemyToSpawn.hp, enemyToSpawn.speed);
+
+            enemiesToSpawn.RemoveAt(0);
+            lastEnemySpawnTime = Time.time;
+        }
+    }
+
+    /// <summary>
+    /// Utworzenie listy wszystkich fal przeciwników
+    /// </summary>
+    private void DefineEnemiesWaves()
+    {
+        allEnemiesWaves.Clear();
+
+        // zestaw 0
+        allEnemiesWaves.Add(new List<List<EnemiesWave>>());
+        // fala 0
+        allEnemiesWaves[0].Add(new List<EnemiesWave>());
+        allEnemiesWaves[0][0].Add(new EnemiesWave(0, 50, 1, 2));
+        // fala 1
+        allEnemiesWaves[0].Add(new List<EnemiesWave>());
+        allEnemiesWaves[0][1].Add(new EnemiesWave(0, 60, 1.25f, 3));
+        // fala 2
+        allEnemiesWaves[0].Add(new List<EnemiesWave>());
+        allEnemiesWaves[0][2].Add(new EnemiesWave(0, 80, 1.25f, 5));
+        // fala 3
+        allEnemiesWaves[0].Add(new List<EnemiesWave>());
+        allEnemiesWaves[0][3].Add(new EnemiesWave(0, 100, 1.5f, 6));
+        // fala 4
+        allEnemiesWaves[0].Add(new List<EnemiesWave>());
+        allEnemiesWaves[0][4].Add(new EnemiesWave(0, 100, 1.5f, 8));
+        // fala 5
+        allEnemiesWaves[0].Add(new List<EnemiesWave>());
+        allEnemiesWaves[0][5].Add(new EnemiesWave(0, 100, 2, 10));
+        // fala 6
+        allEnemiesWaves[0].Add(new List<EnemiesWave>());
+        allEnemiesWaves[0][6].Add(new EnemiesWave(0, 100, 2, 15));
+
+    }
+}
+
+public class EnemiesWave
+{
+    /// <summary>
+    /// Indeks w liście prefabów przeciwników
+    /// </summary>
+    public int prefabId;
+
+    /// <summary>
+    /// Ilość życia przeciwnika
+    /// </summary>
+    public float hp;
+
+    /// <summary>
+    /// Szybkość przeciwnika
+    /// </summary>
+    public float speed;
+
+    /// <summary>
+    /// Ilość przeciwników
+    /// </summary>
+    public int count;
+
+    public EnemiesWave(int prefabId, float hp, float speed, int count = 1)
+    {
+        this.prefabId = prefabId;
+        this.hp = hp;
+        this.speed = speed;
+        this.count = count;
     }
 }
