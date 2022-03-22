f70f1f40312fd58eec495e549b86e800ad3a7033
Luna <73404617+Stefan-Luna-Domaradzki@users.noreply.github.com>
2022-03-22 13:45:35 +0100

    closest-enemy

--- ProjectTowerDefence/Assets/Scripts/Tower.cs
+++ ProjectTowerDefence/Assets/Scripts/Tower.cs
@@ -7,6 +7,8 @@ public class Tower : MonoBehaviour
 {
     private BarController cooldownBar;
 
+    public GameObject thisTower;
+
     protected float damagePerHit, hitRange;
     public float maxCooldown;
     public struct Stats
@@ -19,6 +21,9 @@ public class Tower : MonoBehaviour
     public float hitCooldown, lastHit;
     public Collider[] enemiesInRange;
     private Collider currEnemieToHit;
+
+    private float dist, shortestDist;
+    private int shortesDistIndex;
 
     // Start is called before the first frame update
     void Start()
@@ -58,10 +63,28 @@ public class Tower : MonoBehaviour
     /// </summary>
     private void TowerDealingDamage()
     {
+
         if (Time.time - lastHit >= hitCooldown)
         {
             if (enemiesInRange.Length > 0)
             {
+                //najbliższy przeciwnik
+
+                shortestDist = 0;
+                shortesDistIndex = 0;
+
+                for (int i = 0; i < enemiesInRange.Length; i++)
+                {
+                    dist = Vector3.Distance(thisTower.transform.position, enemiesInRange[i].transform.position);
+                    if (shortestDist > Vector3.Distance(thisTower.transform.position, enemiesInRange[i].transform.position))
+                    {
+                        shortestDist = Vector3.Distance(thisTower.transform.position, enemiesInRange[i].transform.position);
+                        shortestDistIndex = i;
+                        Debug.Log(shortestDistIndex);
+                    }
+                }
+
+
                 // TODO: W przyszłości tutaj trzeba umieścić jakis algorytm, który wybierze
                 // przeciwnika, w którego wieża ma strzelać
                 // Tymczasowo bierze pierwszego z listy
